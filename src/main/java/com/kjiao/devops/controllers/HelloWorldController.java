53f0a3715d963d2cb277cc4727dea03d3ee72785
Kun <kjiao1989@gmail.com>
2017-03-20 12:50:41 +0100

    hello world commit

---
+++ src/main/java/com/kjiao/devops/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.kjiao.devops.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kjiao on 20/03/2017.
+ */
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+}
