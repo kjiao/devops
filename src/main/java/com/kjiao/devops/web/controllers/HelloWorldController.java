fc76fcdb8641735832a2fa1c7fa5bf6c5811a39f
Kun <kjiao1989@gmail.com>
2017-03-20 15:40:59 +0100

    add bootstrap and jquery webjar

--- src/main/java/com/kjiao/devops/web/controllers/HelloWorldController.java
+++ src/main/java/com/kjiao/devops/web/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.kjiao.devops.controllers;
+package com.kjiao.devops.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
